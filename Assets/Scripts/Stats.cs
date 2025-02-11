8b40fa0bc6dd62c258f351517ad9929199e71e88
NeonixGardecier <110261960+NeonixGardecier@users.noreply.github.com>
2025-02-11 21:39:10 +0000

    Stats.cs - Inventory stats

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -56,8 +56,35 @@ public class Stats : MonoBehaviour
         UpdateInventoryStats();
     }
 
+
+     float item_maxHealth; //Displayed as "Health" on stat screens
+     float item_healthRegen; //Displayed as "Vitality"
+     float item_elementalPower; //Sometimes shown as EP
+     float item_strikePower; //acts as physical damage for auto attacks and some passives
+     float item_fortitude; //Lowers elemental damage taken
+     float item_defence; //Lowers strike damage taken
+     float item_fleetness; //Movement speed
+     float item_attackSpeed;
+     float item_projectileSpeed;
+
     void UpdateInventoryStats()
     {
-        
+        idata slot1Data = itemData.GetItemData(Inventory_Slot1);
+        idata slot2Data = itemData.GetItemData(Inventory_Slot2);
+        idata slot3Data = itemData.GetItemData(Inventory_Slot3);
+        idata slot4Data = itemData.GetItemData(Inventory_Slot4);
+        idata slot5Data = itemData.GetItemData(Inventory_Slot5);
+        idata slot6Data = itemData.GetItemData(Inventory_Slot6);
+
+        item_maxHealth       = slot1Data.maxHealth       + slot2Data.maxHealth       + slot3Data.maxHealth       + slot4Data.maxHealth       + slot5Data.maxHealth       + slot6Data.maxHealth;
+        item_healthRegen     = slot1Data.healthRegen     + slot2Data.healthRegen     + slot3Data.healthRegen     + slot4Data.healthRegen     + slot5Data.healthRegen     + slot6Data.healthRegen;
+        item_elementalPower  = slot1Data.elementalPower  + slot2Data.elementalPower  + slot3Data.elementalPower  + slot4Data.elementalPower  + slot5Data.elementalPower  + slot6Data.elementalPower;
+        item_strikePower     = slot1Data.strikePower     + slot2Data.strikePower     + slot3Data.strikePower     + slot4Data.strikePower     + slot5Data.strikePower     + slot6Data.strikePower;
+        item_fortitude       = slot1Data.fortitude       + slot2Data.fortitude       + slot3Data.fortitude       + slot4Data.fortitude       + slot5Data.fortitude       + slot6Data.fortitude;
+        item_defence         = slot1Data.defence         + slot2Data.defence         + slot3Data.defence         + slot4Data.defence         + slot5Data.defence         + slot6Data.defence;
+        item_fleetness       = slot1Data.fleetness       + slot2Data.fleetness       + slot3Data.fleetness       + slot4Data.fleetness       + slot5Data.fleetness       + slot6Data.fleetness;
+        item_attackSpeed     = slot1Data.attackSpeed     + slot2Data.attackSpeed     + slot3Data.attackSpeed     + slot4Data.attackSpeed     + slot5Data.attackSpeed     + slot6Data.attackSpeed;
+        item_projectileSpeed = slot1Data.projectileSpeed + slot2Data.projectileSpeed + slot3Data.projectileSpeed + slot4Data.projectileSpeed + slot5Data.projectileSpeed + slot6Data.projectileSpeed;
+
     }
 }
